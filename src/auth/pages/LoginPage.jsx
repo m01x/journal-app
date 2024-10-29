4a5bc994f3dfdeccbfb38989dd4b1347d309ac02
m01x <fortiz.informatica@gmail.com>
2024-10-28 23:52:25 -0300

    feat(Redux, loginPage): Configuracion de Redux , authSlice y stores para manejo global de la aplicacion.

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,55 +1,84 @@
+import { useDispatch } from 'react-redux';
 import { Link as RouterDomLink } from 'react-router-dom';
-import { Google } from "@mui/icons-material"
 import { Button, Grid, Link, TextField, Typography } from "@mui/material"
+import { Google } from "@mui/icons-material"
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks';
+import { checkingAuthentication, startGoogleSignIn } from '../../store/auth';
 
 export const LoginPage = () => {
+
+  const dispatch = useDispatch();
+
+  const { email, password, onInputChange } = useForm({
+    email: 'flavio@gmail.com',
+    password: '123456'
+  });
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    dispatch(checkingAuthentication( email, password ));
+  }
+
+  const onGoogleSignIn = () => {
+    console.log('onGoogleSignIn');
+    dispatch( startGoogleSignIn() );
+    
+  }
+
   return (
-   <AuthLayout title='Login'>
-
-        <form>
-          <Grid container>
-            <Grid item xs={12} sx={{ mt:2 }}>
-              <TextField 
-              label="Correo" 
-              type="email" 
-              placeholder="correo@google.com" 
+    <AuthLayout title='Login'>
+      <form onSubmit={onSubmit}>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Correo"
+              type="email"
+              placeholder="correo@google.com"
               fullWidth
-              />
-            </Grid>
-
-            <Grid item xs={12} sx={{ mt:2 }}>
-              <TextField 
-              label="Contraseña" 
-              type="password" 
-              placeholder="******" 
-              fullWidth
-              />
-            </Grid>
-
-            <Grid container spacing={ 2 } sx={{ mb : 2 , mt : 1}}>
-              <Grid item xs={ 12 } sm={ 6 }>
-                <Button
-                  variant="contained"
-                  fullWidth
-                >Login</Button>
-              </Grid>
-
-              <Grid item xs={ 12 } sm={ 6 }>
-                <Button
-                  variant="contained"
-                  fullWidth
-                ><Google/>
-                <Typography sx={{ ml:1 }}>Google</Typography></Button>
-              </Grid>
-
-              <Grid container direction={"row"} justifyContent={"end"}>
-                <Link component={RouterDomLink} color="inherit" to="/auth/register">Crear una cuenta</Link>
-              </Grid>
-            </Grid>
-
+              name='email'
+              value={email}
+              onChange={onInputChange}
+            />
           </Grid>
-        </form>
-   </AuthLayout>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Contraseña"
+              type="password"
+              placeholder="******"
+              fullWidth
+              name='password'
+              value={password}
+              onChange={onInputChange}
+            />
+          </Grid>
+
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12} sm={6}>
+              <Button
+                type='submit'
+                variant="contained"
+                fullWidth
+              >Login</Button>
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <Button
+              onClick={ onGoogleSignIn }
+                variant="contained"
+                fullWidth
+              ><Google />
+                <Typography sx={{ ml: 1 }}>Google</Typography></Button>
+            </Grid>
+
+            <Grid container direction={"row"} justifyContent={"end"}>
+              <Link component={RouterDomLink} color="inherit" to="/auth/register">Crear una cuenta</Link>
+            </Grid>
+          </Grid>
+
+        </Grid>
+      </form>
+    </AuthLayout>
   )
 }
